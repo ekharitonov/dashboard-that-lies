f9b2fdcea48ddb1e580e41864924dbda7652594b
Eugene Kharitonov <111708438+ekharitonov@users.noreply.github.com>
2026-04-24 13:56:41 -0500

    data: barrel export

---
+++ src/data.js
@@ -0,0 +1,6 @@
+import { exec_kpis } from './data/exec_kpis.js';
+import { manufacturing } from './data/manufacturing.js';
+import { ecommerce } from './data/ecommerce.js';
+import { saas } from './data/saas.js';
+
+export const DATA = { exec_kpis, manufacturing, ecommerce, saas };

42f342b23a00fd85c4e661a7c06084d287256d09
Eugene Kharitonov <111708438+ekharitonov@users.noreply.github.com>
2026-04-24 18:20:05 -0500

    feat(photo): add author photo as base64 data URI, split in 3 parts for tool-call size limits

--- src/photo.js
+++ src/photo.js
@@ -1,1 +1,5 @@
-export const EUGENE_PHOTO = 'placeholder'; // overridden below
+import { P1 } from './photo_part1.js';
+import { P2 } from './photo_part2.js';
+import { P3 } from './photo_part3.js';
+
+export const EUGENE_PHOTO = 'data:image/jpeg;base64,' + P1 + P2 + P3;
